3e84965ba6b4702b0d2d48890fb011929c9b06f1
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-16 15:23:28 -0600

    added register function

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,4 +3,19 @@ const router = require("express").Router();
 
 const Users = require("../users/users-model.js");
 
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "Could not create user" });
+    });
+});
+
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,27 @@
 const db = require("../database/dbConfig.js");
 
 module.exports = {
-  find
+  find,
+  findById,
+  add
 };
 
 function find() {
   return db("users").select("id", "username");
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username")
+    .where({ id })
+    .first();
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
 }
